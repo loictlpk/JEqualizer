eee53d8d7c261635b7be97b9edc3c33b82e61ca5
Loic Tlpk <tulpinckloic@gmail.com>
2023-09-22 15:21:18 +0200

    UI UPDATE - disabled BTN

--- src/Equalizer.java
+++ src/Equalizer.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Equalizer extends JFrame{
     private JSlider freqSliderLow;
@@ -15,12 +17,66 @@ public class Equalizer extends JFrame{
     private JSlider qSliderHigh;
     private JCheckBox highCheckBox;
 
+    public static void getAllInfos(Frequency l, Frequency m, Frequency h){
+        l.getInfo();
+        m.getInfo();
+        h.getInfo();
+    }
+
     public Equalizer(){
+
+        Frequency low = new Frequency("low",200, 0);
+        Frequency mid = new Frequency("mid",2500, 0);
+        Frequency high = new Frequency("high",12000, 0);
+        getAllInfos(low, mid, high);
+
         setContentPane(eqUi);
         setTitle("JEqualizer");
         setSize(1299, 369);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+        lowCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(lowCheckBox.isSelected()){
+                    low.setEnable(false);
+                    System.out.println("Low Freq OFF");
+                }
+                else{
+                    low.setEnable(true);
+                    System.out.println("Low Freq ON");
+                }
+
+            }
+        });
+        midCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(midCheckBox.isSelected()){
+                    mid.setEnable(false);
+                    System.out.println("Mid Freq OFF");
+                }
+                else{
+                    mid.setEnable(true);
+                    System.out.println("Mid Freq ON");
+                }
+            }
+        });
+        highCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(highCheckBox.isSelected()){
+                    high.setEnable(false);
+                    System.out.println("High Freq OFF");
+                }
+                else{
+                    high.setEnable(true);
+                    System.out.println("High Freq ON");
+                }
+            }
+        });
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,7 @@
 import javax.swing.*;
 
 public class Main {
-    public static void getAllInfos(Frequency l, Frequency m, Frequency h){
-        l.getInfo();
-        m.getInfo();
-        h.getInfo();
-    }
     public static void main(String[] args) {
-
         Equalizer equalizer = new Equalizer();
-
-//       Frequency low = new Frequency("low",200, 0);
-//        low.setEnable(false);
-//        Frequency mid = new Frequency("mid",2500, 0);
-//        mid.setEnable(false);
-//        Frequency high = new Frequency("high",12000, 0);
-//        high.setEnable(false);
-
-
-
-//        getAllInfos(low, mid, high);
-//
-///*
-//        ADDITIONAL OPTIONS
-// */
-//        low.setHzPrecision(100);
-//        low.moreHz();
-////        low.lessHz();
-//
-//        low.setVoltagePrecision(10);
-//        low.moreVoltage();
-////        low.lessVoltage();
-//
-//        low.setWidth(1);
-//        low.moreWidth();
-////        low.lessWidth();
-//
-//        getAllInfos(low, mid, high);
     }
 }
