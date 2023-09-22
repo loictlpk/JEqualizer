ab611f8a2882c3b1abb30d3cc35b965c96b95178
Loic Tlpk <tulpinckloic@gmail.com>
2023-09-22 16:14:58 +0200

    UI UPDATE - ui call

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        SIMPLE UI CALL
+         */
         Equalizer equalizer = new Equalizer();
     }
 }
