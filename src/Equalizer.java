b567d1940301103ef832bdb87dc2c878c3485328
Loic Tlpk <tulpinckloic@gmail.com>
2023-09-22 13:56:00 +0200

    UI IN PROGRESS

--- src/Equalizer.java
+++ src/Equalizer.java
@@ -4,10 +4,13 @@ public class Equalizer {
     private JSlider slider1;
     private JSlider slider2;
     private JSlider slider3;
-    private JCheckBox lowEnableCheckBox;
-    private JCheckBox midEnableCheckBox;
-    private JCheckBox highEnableCheckBox;
+    private JCheckBox disableCheckBox2;
+    private JCheckBox disableCheckBox1;
+    private JCheckBox disableCheckBox;
     private JSlider slider4;
     private JSlider slider5;
     private JSlider slider6;
+    private JSlider slider7;
+    private JSlider slider8;
+    private JSlider slider9;
 }
